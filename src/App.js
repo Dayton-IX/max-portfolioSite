e4ceffacf38abb944b20e1256b115d2666488a4d
Max Dayton <daytonmaximus@gmail.com>
2020-04-28 14:32:12 -0600

    titlecard complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { Component } from 'react';
 import SideBar from './Components/SideBar/SideBar';
+import Titlecard from  './Components/Titlecard/Titlecard';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<SideBar />
+				<main>
+					<Titlecard />
+				</main>
 			</div>
 		)
 	}
