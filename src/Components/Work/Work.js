322d4ec92c6d5544a1820698eacb4f68f7bdfcbb
Max Dayton <daytonmaximus@gmail.com>
2020-04-30 23:03:57 -0600

    working on workitem component

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import classes from './Work.module.css';
 
+import WorkItem from './WorkItem/WorkItem';
+
 const Work = (props) => {
+    const workItems = {
+        CentralValleyFoods: {
+            title: 'Central Valley Foods',
+            text: 'An ecommerce site for farm products, utilizing online shopping features like a cart, order form, and credit card checkout.',
+            tech: ['react', 'redux', 'js', 'node'],
+            link: 'centralvalleyfoods.net',
+            id: 'CentralValleyFoods'
+        }
+    }   
+
     return (
-        <div className={classes.Work}>
+        <div id="work" className={classes.Work}>
             <h1 className={classes.WorkHeader}>My Work.</h1>
+            <div className={classes.WorkItems}>
+                <WorkItem project={workItems.CentralValleyFoods} />
+            </div>
         </div>
     )
 };
--- src/Components/Work/WorkItem/WorkItem.js
+++ src/Components/Work/WorkItem/WorkItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import classes from 'WorkItem.module.css';
+import classes from './WorkItem.module.css';
 
 const WorkItem = (props) => {
     return (
-        <div className={classes.WorkItem} >
-
+        <div id={props.project.id} className={classes.WorkItem} >
+            <div className={classes.WorkItemBackground}>
+                <h4 className={classes.WorkItemHeader}>{props.project.title}</h4>
+                <p className={classes.WorkItemText}>{props.project.text}</p>
+                <span className={classes.WorkItemTech}>Built Using: </span>
+            </div>
         </div>
     )
 };
