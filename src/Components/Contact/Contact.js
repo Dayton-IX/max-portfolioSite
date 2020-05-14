8d161f232b3cb336812097b7c23752c454b3eafa
Max Dayton <daytonmaximus@gmail.com>
2020-05-14 01:14:21 -0600

    contact form html

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Contact.module.css';
 
-const Contact = (props) => {
-    return (
-        <div id="contact" className={classes.Contact}>
-            <h1 className={classes.ContactHeader}>Get In Touch.</h1>
-        </div>
-    )
+class Contact extends Component {
+    render() {
+        return (
+            <div id="contact" className={classes.Contact}>
+                <h1 className={classes.ContactHeader}>Get In Touch.</h1>
+                <div className={classes.ContactMain}>
+                    <div className={classes.ContactLeft}>
+                        <div className={classes.ContactFormContainer}>
+                            <form className={classes.ContactForm}>
+                                <input type="text" placeholder="Name" />
+                                <input type="text" placeholder="Email" />
+                                <textarea placeholder="Message" />
+                                <input type="submit" value="Send Me An Email!" />
+                            </form>
+                            <div className={classes.FollowMe}>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 };
 
 export default Contact;
