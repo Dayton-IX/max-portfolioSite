23329e076cbcf9649ea0207f80156dafc56a290c
Max Dayton <daytonmaximus@gmail.com>
2020-05-30 07:26:14 -0600

    covid styling changes, error handling changes

--- src/COVID/Layout/Layout.js
+++ src/COVID/Layout/Layout.js
@@ -6,11 +6,11 @@ class Layout extends Component {
     render() {
         return (
             <div className={classes.Layout}>
-                <h1>COVID-19</h1>
-                <h2>Coronavirus</h2>
-                <main>
+                <h1>COVID-19 Tracker</h1>
+                <h2>Data from <a href="https://coronavirus.jhu.edu/" target="blank">Johns Hopkins</a></h2>
+                <div>
                     {this.props.children}
-                </main>
+                </div>
             </div>
         )
     }
--- src/COVID/Stats/Stats.js
+++ src/COVID/Stats/Stats.js
@@ -5,16 +5,15 @@ import classes from './Stats.module.css';
 const Stats = (props) => {
     const { stats, loading, error } = useStats(props.url);
     console.log(stats, loading, error);
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error...</p>;
     return (
         <div className={classes.Stats} >
-            <h3>Confirmed:</h3>
-            <span>{stats.confirmed.value}</span>
+            <h2>{props.title}</h2>
+            <h3>Confirmed Cases:</h3>
+            {loading ? <p>Loading...</p> : error ? <p>There Was an Error getting this data, please try again...</p> : <span>{stats.confirmed.value}</span>}
             <h3>Deaths:</h3>
-            <span>{stats.deaths.value}</span>
+            {loading ? <p>Loading...</p> : error ? <p>There Was an Error getting this data, please try again...</p> : <span>{stats.deaths.value}</span>}
             <h3>Recovered:</h3>
-            <span>{stats.recovered.value}</span>
+            {loading ? <p>Loading...</p> : error ? <p>There Was an Error getting this data, please try again...</p> : <span>{stats.recovered.value}</span>}
         </div>
     )
 };
