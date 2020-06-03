349706588fd803e4d8bb75c1e6035204d9bc7412
Max Dayton <daytonmaximus@gmail.com>
2020-06-03 17:17:44 -0600

    fixed svg widths

--- src/USES/Uses.js
+++ src/USES/Uses.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import UnderConstruction from './underConstruction/underConstruction';
+// import UnderConstruction from './underConstruction/underConstruction';
 import classes from './Uses.module.css';
+import HardwareList from './lists/HardwareList';
+import SoftwareList from './lists/SoftwareList';
+import DrumsList from './lists/DrumsList';
 
 class Uses extends Component {
     state = {
@@ -32,6 +35,11 @@ class Uses extends Component {
                         <button className={this.state.hardActive ? classes.UsesLinkActive : classes.UsesLink} onClick={this.hardClicked}>Hardware</button>
                         <button className={this.state.softActive ? classes.UsesLinkActive : classes.UsesLink} onClick={this.softClicked}>Software</button>
                         <button className={this.state.drumActive ? classes.UsesLinkActive : classes.UsesLink} onClick={this.drumClicked}>Drums</button>
+                    </div>
+                </div>
+                <div className={classes.UsesMain} >
+                    <div className={classes.MenuItems}>
+                        {this.state.hardActive ? <HardwareList /> : this.state.softActive ? <SoftwareList /> : <DrumsList />}
                     </div>
                 </div>
             </div>
