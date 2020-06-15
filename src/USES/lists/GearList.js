b8eaf92f873dee2e2603fed666e0a5035af94d94
Max Dayton <daytonmaximus@gmail.com>
2020-06-14 21:23:50 -0600

    hardware list completed

--- src/USES/lists/GearList.js
+++ src/USES/lists/GearList.js
@@ -5,8 +5,6 @@ import UsesItem from '../UsesItem/UsesItem';
 const GearList = (props) => {
     return (
         <div>
-            <h1>Gear</h1>
-            <UsesItem />
         </div>
     )
 };
--- src/USES/lists/SoftwareList.js
+++ src/USES/lists/SoftwareList.js
@@ -5,8 +5,6 @@ import UsesItem from '../UsesItem/UsesItem';
 const SoftWareList = (props) => {
     return (
         <div>
-            <h1>Software</h1>
-            <UsesItem />
         </div>
     )
 };
