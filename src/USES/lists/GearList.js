0d3ed5b85e5a9d9586f88ff12a1775b508241251
Max Dayton <daytonmaximus@gmail.com>
2021-02-11 23:16:32 -0700

    titlecard responsive fix, uses updates

--- src/USES/lists/GearList.js
+++ src/USES/lists/GearList.js
@@ -7,13 +7,15 @@ const GearList = (props) => {
     const phoneCase = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M272 0H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h224c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zM160 480c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm112-108c0 6.6-5.4 12-12 12H60c-6.6 0-12-5.4-12-12V60c0-6.6 5.4-12 12-12h200c6.6 0 12 5.4 12 12v312z"/></svg>
     const bag = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M128 480h256V80c0-26.5-21.5-48-48-48H176c-26.5 0-48 21.5-48 48v400zm64-384h128v32H192V96zm320 80v256c0 26.5-21.5 48-48 48h-48V128h48c26.5 0 48 21.5 48 48zM96 480H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48h48v352z"/></svg>
     const headphones = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M160 288h-16c-35.35 0-64 28.7-64 64.12v63.76c0 35.41 28.65 64.12 64 64.12h16c17.67 0 32-14.36 32-32.06V320.06c0-17.71-14.33-32.06-32-32.06zm208 0h-16c-17.67 0-32 14.35-32 32.06v127.88c0 17.7 14.33 32.06 32 32.06h16c35.35 0 64-28.71 64-64.12v-63.76c0-35.41-28.65-64.12-64-64.12zM256 32C112.91 32 4.57 151.13 0 288v112c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16V288c0-114.67 93.33-207.8 208-207.82 114.67.02 208 93.15 208 207.82v112c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16V288C507.43 151.13 399.09 32 256 32z"/></svg>
+    const wallet = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M461.2 128H80c-8.84 0-16-7.16-16-16s7.16-16 16-16h384c8.84 0 16-7.16 16-16 0-26.51-21.49-48-48-48H64C28.65 32 0 60.65 0 96v320c0 35.35 28.65 64 64 64h397.2c28.02 0 50.8-21.53 50.8-48V176c0-26.47-22.78-48-50.8-48zM416 336c-17.67 0-32-14.33-32-32s14.33-32 32-32 32 14.33 32 32-14.33 32-32 32z"/></svg>
 
     return (
         <span>
             <UsesItem icon={drum} title="Drum Set" name="Mapex Armory 5 Piece" text="My Matte Green Mapex 5 Peice with Zildjian Cymbals." />
-            <UsesItem icon={phoneCase} title="Phone Case" name="Rhinoshield CrashGuard" text="A nice, lightweight, bumper guard that protects my phone and makes it easier to grip." />
+            <UsesItem icon={phoneCase} title="Phone Case" name="Apple clear Magsafe case" text="Extends the Magsafe for use with other Magsafe accessories" />
             <UsesItem icon={bag} title="Bag" name="Handcrafted Leather Laptop Bag" text="A nice, high quality, leather, laptop bag made by a small handcrafting shop in Fussen, Germany." />
-            <UsesItem icon={headphones} title="Earbuds" name="Galaxy Buds+ " text="A pair of black, wireless, bluetooth earbuds with touch gestures and noise cancellation." />
+            <UsesItem icon={wallet} title="Wallet" name="Apple Magsafe card wallet" text="A black card wallet that sticks to the back of my phone via magsafe!" />
+            <UsesItem icon={headphones} title="Earbuds" name="AirPods Pro" text="Crazy good connectivity with Apple products and great noise cancellation and transparency modes!" />
         </span>
     )
 };
